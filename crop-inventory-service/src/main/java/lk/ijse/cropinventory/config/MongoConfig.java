f24584276484d6817f26881ee87387518c2549e1
Vinod663 <vinodfernando048@gmail.com>
2026-03-08 00:28:23 +0530

    Create the Mongo Configuration Class

---
+++ crop-inventory-service/src/main/java/lk/ijse/cropinventory/config/MongoConfig.java
@@ -0,0 +1,13 @@
+package lk.ijse.cropinventory.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
+
+@Configuration
+public class MongoConfig extends AbstractMongoClientConfiguration {
+
+    @Override
+    protected String getDatabaseName() {
+        return "crop_inventory_db"; // THIS FORCES THE CORRECT DATABASE NAME!
+    }
+}
