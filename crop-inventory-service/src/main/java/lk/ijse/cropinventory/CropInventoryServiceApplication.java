adcd244b3ffed76ca4184e32b7ec145628c99082
Vinod663 <vinodfernando048@gmail.com>
2026-03-08 00:19:26 +0530

    strictly use explicit mongodb uri to bypass test default

--- crop-inventory-service/src/main/java/lk/ijse/cropinventory/CropInventoryServiceApplication.java
+++ crop-inventory-service/src/main/java/lk/ijse/cropinventory/CropInventoryServiceApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 @EnableDiscoveryClient
 public class CropInventoryServiceApplication {
-
+//
     public static void main(String[] args) {
         SpringApplication.run(CropInventoryServiceApplication.class, args);
     }
