b8f714735a53d7a4d0704c300ed12cd2d0b98884
Vinod663 <vinodfernando048@gmail.com>
2026-03-08 15:50:25 +0530

    add authentication dtos and update feign client for external login

--- zone-management-service/src/main/java/lk/ijse/zonemanagement/client/ExternalIoTClient.java
+++ zone-management-service/src/main/java/lk/ijse/zonemanagement/client/ExternalIoTClient.java
@@ -1,5 +1,7 @@
 package lk.ijse.zonemanagement.client;
 
+import lk.ijse.zonemanagement.dto.AuthRequest;
+import lk.ijse.zonemanagement.dto.AuthResponse;
 import lk.ijse.zonemanagement.dto.DeviceRegistrationRequest;
 import lk.ijse.zonemanagement.dto.DeviceRegistrationResponse;
 import org.springframework.cloud.openfeign.FeignClient;
@@ -9,6 +11,9 @@ import org.springframework.web.bind.annotation.RequestHeader;
 
 @FeignClient(name = "external-iot-service", url = "http://104.211.95.241:8080/api")
 public interface ExternalIoTClient {
+
+    @PostMapping("/auth/login")
+    AuthResponse login(@RequestBody AuthRequest request);
 
     @PostMapping("/devices")
     DeviceRegistrationResponse registerDevice(
