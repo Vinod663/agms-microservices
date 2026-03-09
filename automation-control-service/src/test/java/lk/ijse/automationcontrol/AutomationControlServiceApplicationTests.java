b72349a2d7f7971c3ef72c9d2f698b6820b1928d
Vinod663 <vinodfernando048@gmail.com>
2026-03-09 18:13:53 +0530

    add automation-control-service properties with port 8083 and database connection

---
+++ automation-control-service/src/test/java/lk/ijse/automationcontrol/AutomationControlServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.automationcontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomationControlServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
